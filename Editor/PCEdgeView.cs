12a00c42e80c013fdaf21530cb0d00489f92eefe
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2024-02-23 05:52:32 +0900

    Elements deletion

--- Editor/PCEdgeView.cs
+++ Editor/PCEdgeView.cs
@@ -16,6 +16,12 @@ public class PCEdgeView : Edge
         input = edge.input;
         onEdgeSelected = action;
     }
+    public PCEdgeView(Port input, Port output, Transition transition, Action<PCEdgeView> action){
+        this.transition = transition;
+        this.output = output;
+        this.input = input;
+        onEdgeSelected = action;
+    }
     public override void OnSelected()
     {
         base.OnSelected();
--- Editor/PCGraphView.cs
+++ Editor/PCGraphView.cs
@@ -25,6 +25,7 @@ public class PCGraphView : GraphView
         nodeViews = new Dictionary<string, PCNodeView>();
         // set callbacks
         SetOnGraphViewChanged();
+        SetElementsDeletion();
     }
     private void CreateGridBackground(){
         GridBackground gridBackground = new GridBackground();
@@ -79,8 +80,9 @@ public class PCGraphView : GraphView
         PCNodeView output = nodeViews[node.guid];
         foreach(var t in node.transition){
             PCNodeView input = nodeViews[t.dest.guid];
-            Edge edge = output.outputPort.ConnectTo(input.inputPort);
-            PCEdgeView edgeView = new PCEdgeView(edge, t, onEdgeSelected);
+            PCEdgeView edgeView = new PCEdgeView(input.inputPort, output.outputPort, t, onEdgeSelected);
+            output.outputPort.Connect(edgeView);
+            input.inputPort.Connect(edgeView);
             AddElement(edgeView);          
         }       
     }
@@ -126,22 +128,79 @@ public class PCGraphView : GraphView
     }
 #endregion Create Elements
 
+#region Delete Elements
+    // Modify Assets
+    private void DeleteNode(PCNodeView nodeView){
+        // undo
+        DisconnectAll(nodeView);
+        entryNode.nodes.Remove(nodeView.node);
+        nodeViews.Remove(nodeView.node.guid);
+        if(!Application.isPlaying){
+            AssetDatabase.RemoveObjectFromAsset(nodeView.node);
+            AssetDatabase.SaveAssets();
+        }
+    }
+    // Modify Assets
+    private void DeleteEdge(PCEdgeView edge){
+        // undo
+        PCNodeView nodeView = edge.output.node as PCNodeView;
+        nodeView.node.transition.Remove(edge.transition);
+        AssetDatabase.SaveAssets();
+    }
+    private void DisconnectAll(PCNodeView nodeView){
+        if(nodeView.inputPort != null && nodeView.inputPort.connected){
+            foreach(Edge edge in nodeView.inputPort.connections){
+                DeleteEdge(edge as PCEdgeView);
+            }
+            DeleteElements(nodeView.inputPort.connections);
+        }
+        if(nodeView.outputPort != null && nodeView.outputPort.connected){
+            foreach(Edge edge in nodeView.outputPort.connections){
+                DeleteEdge(edge as PCEdgeView);
+            }
+            DeleteElements(nodeView.outputPort.connections);
+        }
+    }
+#endregion Delete Elements
+
 #region Callbacks
 // create edge & move elements
-private void SetOnGraphViewChanged(){
-    graphViewChanged = (changes) => {
-        if(changes.edgesToCreate != null){
-            for(int i = 0; i<changes.edgesToCreate.Count;i++){
-                PCNodeView input = changes.edgesToCreate[i].input.node as PCNodeView;
-                PCNodeView output = changes.edgesToCreate[i].output.node as PCNodeView;
-                Transition transition = AddTransition(input, output);
-                changes.edgesToCreate[i] = new PCEdgeView(changes.edgesToCreate[i], transition, onEdgeSelected);
+    private void SetOnGraphViewChanged(){
+        graphViewChanged = (changes) => {
+            if(changes.edgesToCreate != null){
+                for(int i = 0; i<changes.edgesToCreate.Count;i++){
+                    PCNodeView input = changes.edgesToCreate[i].input.node as PCNodeView;
+                    PCNodeView output = changes.edgesToCreate[i].output.node as PCNodeView;
+                    Transition transition = AddTransition(input, output);
+                    changes.edgesToCreate[i] = new PCEdgeView(changes.edgesToCreate[i], transition, onEdgeSelected);
+                }
             }
-        }
+            if(changes.elementsToRemove != null){
+                foreach(var element in changes.elementsToRemove){
+                    if(element is PCEdgeView edge){
+                        DeleteEdge(edge);
+                    }
+                }
+            }
+            return changes;
+        };
+    }
 
-        return changes;
-    };
-}
+    private void SetElementsDeletion(){
+        deleteSelection = (operationName, askUser) => {
+            List<GraphElement> deleteElements = new List<GraphElement>();
+            foreach(GraphElement element in selection){
+                if(element is PCNodeView nodeView){
+                    if(nodeView.node is PlayerControllerAsset) continue;
+                    else{
+                        DeleteNode(nodeView);
+                    }
+                }
+                deleteElements.Add(element);
+            }
+            DeleteElements(deleteElements);
+        };
+    }
 #endregion Callbacks
 
 #region Utility
